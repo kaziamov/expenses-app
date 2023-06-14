1d286a3d14094effa85ee75c517c51eb5e89b2b3
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-15 02:07:59 +0300

    Refactor code. Comment old code to remove

--- expenses_app/__init__.py
+++ expenses_app/__init__.py
@@ -1,13 +1,17 @@
-from flask import Flask
-from datetime import timedelta
-from expenses_app.views import main
+from .api.app import app
+
+__all__ = ['app']
+
+# from flask import Flask
+# from datetime import timedelta
+# from expenses_app.views import main
 
 
-app = Flask(__name__)
-app.secret_key = 'test'
-app.url_map.strict_slashes = False
-app.permanent_session_lifetime = timedelta(hours=24)
-app.register_blueprint(main)
+# app = Flask(__name__)
+# app.secret_key = 'test'
+# app.url_map.strict_slashes = False
+# app.permanent_session_lifetime = timedelta(hours=24)
+# app.register_blueprint(main)
 
 
 if __name__ == '__main__':
