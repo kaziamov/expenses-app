b3841253af6166fa561c66f1defa0c0921c7b755
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-15 02:08:56 +0300

    Add routes

---
+++ expenses_app/api/v1/routes.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from .controllers import get_expenses, add_expense, edit_expense
+from .schemas import ExpenseSchema
+
+
+
+router = APIRouter()
+
+
+@router.get('/expenses')
+async def expenses():
+    return await edit_expense()
+
+
+@router.post('/expenses/create')
+async def expense_create(item: ExpenseSchema):
+    data = dict(item)
+    return await add_expense(data)
+
+
+@router.patch('/expenses/{exp_id}/edit')
+async def expense_edit(exp_id: str):
+    return
+
+
+
+@router.put('/expenses/{exp_id}/edit')
+async def expense_delete(exp_id: str):
+    return await edit_expense(exp_id)
