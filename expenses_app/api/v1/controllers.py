c5269bb8948d872b9419a03b8c70f3d607cebb94
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-15 02:08:45 +0300

    Add dummy controllers

---
+++ expenses_app/api/v1/controllers.py
@@ -0,0 +1,16 @@
+
+
+async def get_expenses():
+    pass
+
+
+async def add_expense(data):
+    print(data)
+
+
+async def edit_expense(exp_id, data):
+    pass
+
+
+async def delete_expense(exp_id):
+    pass
