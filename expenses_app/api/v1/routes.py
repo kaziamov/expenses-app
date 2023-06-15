1c8d604c37c7d015b916c116871dc34053b5e595
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-15 14:31:03 +0300

    Fix linter errors

--- expenses_app/api/v1/routes.py
+++ expenses_app/api/v1/routes.py
@@ -3,27 +3,25 @@ from .controllers import get_expenses, add_expense, edit_expense
 from .schemas import ExpenseSchema
 
 
-
 router = APIRouter()
 
 
-@router.get('/expenses')
+@router.get("/expenses")
 async def expenses():
-    return await edit_expense()
+    return await get_expenses()
 
 
-@router.post('/expenses/create')
+@router.post("/expenses/create")
 async def expense_create(item: ExpenseSchema):
     data = dict(item)
     return await add_expense(data)
 
 
-@router.patch('/expenses/{exp_id}/edit')
+@router.patch("/expenses/{exp_id}/edit")
 async def expense_edit(exp_id: str):
     return
 
 
-
-@router.put('/expenses/{exp_id}/edit')
+@router.put("/expenses/{exp_id}/edit")
 async def expense_delete(exp_id: str):
     return await edit_expense(exp_id)
