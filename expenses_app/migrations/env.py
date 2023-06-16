42ee2cd17b185b4afe440a645206d76ad437ead0
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-17 01:06:50 +0300

    Add db config

--- expenses_app/migrations/env.py
+++ expenses_app/migrations/env.py
@@ -5,10 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 from expenses_app import models
+from expenses_app.settings import DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
