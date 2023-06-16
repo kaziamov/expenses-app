9002ff491657a3b988d7be501edb2442c5b7811b
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-17 01:07:19 +0300

    Refactor code. Remove old code

--- expenses_app/__init__.py
+++ expenses_app/__init__.py
@@ -1,7 +1,3 @@
-from .api.app import app
-
-__all__ = ['app']
-
 # from flask import Flask
 # from datetime import timedelta
 # from expenses_app.views import main
@@ -11,8 +7,4 @@ __all__ = ['app']
 # app.secret_key = 'test'
 # app.url_map.strict_slashes = False
 # app.permanent_session_lifetime = timedelta(hours=24)
-# app.register_blueprint(main)
-
-
-if __name__ == '__main__':
-    app()
+# app.register_blueprint(main)if __name__ == '__main__':    app()
