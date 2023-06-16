81215ed331d7e586809e635dd1acbfc4160cb9e1
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-17 00:15:37 +0300

    Update import

--- expenses_app/api/v1/routes.py
+++ expenses_app/api/v1/routes.py
@@ -1,27 +1,43 @@
 from fastapi import APIRouter
-from .controllers import get_expenses, add_expense, edit_expense
+
 from .schemas import ExpenseSchema
+from . import controllers
 
 
 router = APIRouter()
 
 
 @router.get("/expenses")
-async def expenses():
-    return await get_expenses()
+async def get_expenses():
+    return await controllers.get_expenses()
 
 
 @router.post("/expenses/create")
-async def expense_create(item: ExpenseSchema):
+async def create_expense(item: ExpenseSchema):
     data = dict(item)
-    return await add_expense(data)
+    return await controllers.add_expense(data)
 
 
 @router.patch("/expenses/{exp_id}/edit")
-async def expense_edit(exp_id: str):
+async def edit_expense(exp_id: str):
     return
 
 
-@router.put("/expenses/{exp_id}/edit")
-async def expense_delete(exp_id: str):
-    return await edit_expense(exp_id)
+@router.delete("/expenses/{exp_id}/delete")
+async def delete_expense(exp_id: str):
+    return await controllers.delete_expense(exp_id)
+
+
+@router.get("/categories")
+async def get_categories():
+    return await controllers.get_categories()
+
+
+@router.post("/categories/create")
+async def create_category():
+    return await controllers.get_categories()
+
+
+@router.patch("/categories/{exp_id}/edit")
+async def edit_category(exp_id: str):
+    return await controllers.edit_category(exp_id)
