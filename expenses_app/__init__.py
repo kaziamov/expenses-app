79a31dad1326c89df633a7d328571dc26f1bc8d7
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-18 00:06:58 +0300

    Remove old parts of code

--- expenses_app/__init__.py
+++ expenses_app/__init__.py
@@ -1,11 +1,1 @@
-# from flask import Flask
-# from datetime import timedelta
-# from expenses_app.views import main
 from .app import app
-
-
-# app = Flask(__name__)
-# app.secret_key = 'test'
-# app.url_map.strict_slashes = False
-# app.permanent_session_lifetime = timedelta(hours=24)
-# app.register_blueprint(main)if __name__ == '__main__':    app()
