c81067497117ffd6884b83692f9bb6615a89a4d4
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-18 00:03:13 +0300

    Add async and db url variables

--- expenses_app/settings.py
+++ expenses_app/settings.py
@@ -27,4 +27,6 @@ else:
     DB_PASS = os.getenv("PG_PASSWORD")
     DB_HOST = os.getenv("PG_HOST")
     DB_PORT = os.getenv("PG_PORT")
-    DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+ASYNC_DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
