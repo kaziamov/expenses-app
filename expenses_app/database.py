253e0a1fd4062624a9720bb20193965b876e9b2a
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-18 00:00:23 +0300

    Refactor code. Remove old parts of code

--- expenses_app/database.py
+++ expenses_app/database.py
@@ -1,50 +1,33 @@
-import asyncpg
-import asyncio
 from contextlib import asynccontextmanager
-from typing import Optional
-from settings import *
+from .settings import ASYNC_DATABASE_URL, MIN_CONN, MAX_CONN
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-_pool: Optional[asyncpg.pool.Pool] = None
-_pool_lock: Optional[asyncio.Lock] = None
-
-
-def init_pool_lock():
-    global _pool_lock
-    _pool_lock = asyncio.Lock()
-
-
-async def connection_init(conn):
-    return conn
-
-
-async def connect() -> asyncpg.Pool:
-    global _pool
-    async with _pool_lock:
-        if not _pool:
-            _pool = await asyncpg.create_pool(
-                init=connection_init,
-                database=DB_NAME,
-                user=DB_USER,
-                password=DB_PASS,
-                host=DB_HOST,
-                post=DB_PORT,
-                min_size=MIN_CONN,
-                max_size=MAX_CONN,
-            )
-    return _pool
-
-
-async def close():
-    global _pool
-    if not _pool:
-        return
-    async with _pool_lock:
-        await _pool.close()
+engine = create_async_engine(ASYNC_DATABASE_URL, pool_size=MIN_CONN, max_overflow=MAX_CONN)
+async_session = sessionmaker(engine, class_=AsyncSession)
 
 
 @asynccontextmanager
-async def get_connection():
-    conn_pool = await connect()
-    conn = await conn_pool.acquire()
-    yield conn
-    await conn_pool.release(conn)
+async def get_connection() -> AsyncSession:
+    async with async_session() as session:
+        yield session
+
+
+class BaseConnection:
+    pass
+    def __init__(self) -> None:
+        pass
+
+    async def get_connect(self):
+        async with async_session() as session:
+            yield session
+
+
+class AlchemyConnection(BaseConnection):
+    def __init__(self) -> None:
+        self.engine = create_async_engine(ASYNC_DATABASE_URL, pool_size=MIN_CONN, max_overflow=MAX_CONN)
+        self.async_session = sessionmaker(engine, class_=AsyncSession)
+
+    async def get_connect(self):
+        async with self.async_session() as session:
+            yield session
