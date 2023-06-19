6437b513eec17703c2f9283b7671de7b87d684e8
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-19 21:31:57 +0300

    Fix import path

--- expenses_app/app.py
+++ expenses_app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from api.v1.routes import router as v1_routes
+from .api.v1.routes import router as v1_routes
 
 app = FastAPI()
 app.include_router(v1_routes, prefix="/api/v1")
