4201af59bc449620eadce92e7d22d94be0a01f4c
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-20 13:54:15 +0300

    Add logging

--- expenses_app/controllers.py
+++ expenses_app/controllers.py
@@ -1,15 +1,19 @@
 from .database import engine, get_connection
 from .models import CategoryModel
+from . import logger
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
 
 
+@logger.log_this_output
 async def get_categories() -> dict:
     async with engine.connect() as conn:
         results = await conn.execute(select(CategoryModel))
-    return {k: v for k, v in results.all()}
+    serialized = {k: v for k, v in results.all()}
+    return serialized
 
 
+@logger.log_this_input_and_output
 async def add_category(data: dict):
     new_category = CategoryModel(**data)
     async with get_connection() as conn:
