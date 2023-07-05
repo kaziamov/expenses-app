fb788e25e410ea7c3f5094413dce98647e1b35f2
Ilia Kaziamov <kaziamov@outlook.com>
2023-07-05 22:16:52 +0300

    Add basic logging

---
+++ expenses_app/api/v1/logger.py
@@ -0,0 +1,37 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO, format="%(levelname)s:     %(asctime)s - %(message)s"
+)
+
+logger = logging.getLogger("expenses_app")
+
+
+def log_this_input_and_output(func):
+    async def inner(*args, **kwargs):
+        logger.info("[{}.{}] input: {}, {}".format(
+            (func.__module__),
+            (func.__name__),
+            "".join(*args),
+            kwargs
+        ))
+        result = await func(*args, **kwargs)
+        logger.info("[{}.{}] output: {}".format(
+            (func.__module__),
+            (func.__name__),
+            result
+        ))
+        return result
+    return inner
+
+
+def log_this_output(func):
+    async def inner(*args, **kwargs):
+        result = await func(*args, **kwargs)
+        logger.info("[{}.{}] output: {}".format(
+            (func.__module__),
+            (func.__name__),
+            result
+        ))
+        return result
+    return inner
+
